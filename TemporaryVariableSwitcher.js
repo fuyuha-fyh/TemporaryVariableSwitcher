e8feb03fa9a12a5600e82943f9d0ee390826b2f5
fuyuha-fyh <115709508+fuyuha-fyh@users.noreply.github.com>
2023-11-23 06:55:33 +0900

    fix: 2023-11-23
    ちょっとした修正を行いました。

--- TemporaryVariableSwitcher.js
+++ TemporaryVariableSwitcher.js
@@ -4,9 +4,9 @@
 
 /*:
  * @plugindesc マップごとに一時変数を切り替えるやつ
- * @author 冬派 
+ * @author 冬派
  *
- * @param tempVariablesIds 
+ * @param tempVariablesIds
  * @text 一時変数として扱う変数番号
  * @desc 一時変数として扱う変数の番号を指定します
  * @type string[]
@@ -15,71 +15,68 @@
  */
 
 $tvsData = {};
-(function($) {
+(function ($) {
 	$.params = PluginManager.parameters("TemporaryVariableSwitcher") || {};
 	$.tempVariablesIds = JSON.parse($.params["tempVariablesIds"]) || [];
 	$.mapData = $.mapData || {};
 
 	const _Game_Player_performTransfer = Game_Player.prototype.performTransfer;
-	Game_Player.prototype.performTransfer = function() {
+	Game_Player.prototype.performTransfer = function () {
 		$.tempVariablesIds.forEach((num) => {
 			let id = parseInt(num);
 			if (!isNaN(id)) {
 				let variableValue = $gameVariables.value(id);
 				if (variableValue) {
-					if (!($.mapData[$gameMap.mapId()])) $.mapData[$gameMap.mapId()] = {};
+					if (!$.mapData[$gameMap.mapId()]) $.mapData[$gameMap.mapId()] = {};
 					$.mapData[$gameMap.mapId()][id] = variableValue;
 				}
 			}
 		});
 
 		let newMapData = $.mapData[this._newMapId];
-		if (newMapData) {
-			$.tempVariablesIds.forEach((num) => {
-				let id = parseInt(num);
-				if (!isNaN(id)) {
+		$.tempVariablesIds.forEach((num) => {
+			let id = parseInt(num);
+			if (!isNaN(id)) {
+				if (newMapData) {
 					if (newMapData[id]) $gameVariables.setValue(id, newMapData[id]);
 					else $gameVariables.setValue(id, 0);
+				} else {
+					$gameVariables.setValue(id, 0);
 				}
-			});
-		} else {
-			$.tempVariablesIds.forEach((num) => {
-				let id = parseInt(num);
-				if (!isNaN(id)) $gameVariables.setValue(id, 0);
-			});
-		}
+			}
+		});
 
 		_Game_Player_performTransfer.call(this);
 	};
 
 	const _DataManager_makeSaveContents = DataManager.makeSaveContents;
-	DataManager.makeSaveContents = function() {
+	DataManager.makeSaveContents = function () {
 		let contents = _DataManager_makeSaveContents.call(this);
 		contents.tvsMapData = $.mapData || {};
 		return contents;
 	};
 
 	const _DataManager_extractSaveContents = DataManager.extractSaveContents;
-	DataManager.extractSaveContents = function(contents) {
+	DataManager.extractSaveContents = function (contents) {
 		_DataManager_extractSaveContents.call(this, contents);
 		$.mapData = contents.tvsMapData || {};
 	};
 
-	$.clearVariable = function(mapId) {
+	$.clearVariable = function (mapId) {
 		let mapData = $.mapData[mapId];
-		if (mapData) $.mapData[mapId] = {}; 
-	}
+		if (mapData) $.mapData[mapId] = {};
+	};
 
-	$.clearAllVariable = function() {
+	$.clearAllVariable = function () {
 		$.mapData = {};
-	}
+	};
 
-	$.setVariable = function(mapId, variableId, variableValue) {
+	$.setVariable = function (mapId, variableId, variableValue) {
 		let mapData = $.mapData[mapId];
 		if (mapData) {
 			variableId = parseInt(variableId);
-			if (!isNaN(variableId) && mapData[variableId]) mapData[variableId] = variableValue;
+			if (!isNaN(variableId) && mapData[variableId])
+				mapData[variableId] = variableValue;
 		}
-	}
-
+	};
 })($tvsData);
